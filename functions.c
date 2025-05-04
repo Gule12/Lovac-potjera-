0d60f8fc6462f80c1a514b94bb91b97955b53f8e
Gule12 <malodrvo4@gmail.com>
2025-05-04 18:19:19 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,5 +1,34 @@
-#include <stdio.h>
+#include "functions.h"
+#include <stdlib.h>
 
-void greet() {
-	printf("Hello World");
+QUESTION* question;
+PLAYER* player;
+
+int allocateQuestions() {
+	question = (QUESTION*)calloc(10, sizeof(QUESTION)); 
+
+	if (question == NULL) {
+		perror("allocateQuestion");
+		return 1;
+	}
+	return 0;
+}
+
+int allocatePlayer() {
+	 player = (PLAYER*)calloc(1, sizeof(PLAYER));
+
+	if (player == NULL) {
+		perror("allocatePlayer");
+		return 1;
+	}
+	return 0;
+}
+
+void freeQuestions() {
+
+	free(question);
+}
+
+void freePlayer() {
+	free(player);
 }
