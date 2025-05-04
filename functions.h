642c7ad98e6554db20c4abbf0c90271a289483c0
Gule12 <malodrvo4@gmail.com>
2025-05-04 18:20:19 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,11 +1,38 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+typedef struct {
+	char name[8];
+	int score;
+}PLAYER;
+
+typedef struct {
+	char question[100];
+	char correctOption[100];
+	char wrongOption1[100];
+	char wrongOption2[100];
+	char wrongOption3[100];
+	char corretAnswer;
+}QUESTION;
 
 
-void greet();
+
+int allocateQuestions();
+int allocatePlayer();
+void freeQuestions();
+void freePlayer();
 
 
+extern QUESTION* question;
+extern PLAYER* player;
+
+
+enum Opcije {
+	Igraj = 1,
+	DodajIgraca,
+	IzbrisiIgraca,
+	Izlaz
+};
 
 
 #endif 
