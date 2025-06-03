dca35266f5a0579cd68cd65dcec47b571cca8a7a
Gule12 <malodrvo4@gmail.com>
2025-06-03 20:52:29 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,35 +1,79 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include "functions.h"
+#include "funkcije.h"
 #include <stdio.h>
+#include <string.h>
+
 int main() {
-	printf("1. IGRAJ\n");
-	printf("2. DODAJ IGRACA\n");
-	printf("3. IZBRISI IGRACA\n");
-	printf("4. IZLAZ\n");
+    printf("%s", pocetniTekst);
+    static int izbor;
+    char imeIgraca[100];
+    int rezultat = 0;
 
-	int choice;
-	scanf("%d", &choice);
+    if (zauzmiMemorijuPitanja() != 0 || zauzmiMemorijuPitanja() != 0) {
+        printf("Neuspjesno zauzimanje memorije\n");
+        return 1;
+    }
 
+    while (1) {
+        printf("\n-----LOVAC-----\n");
+        printf("1. IGRAJ\n");
+        printf("2. DODAJ IGRACA\n");
+        printf("3. IZBRISI IGRACA\n");
+        printf("4. ISPIS IGRACA\n");
+        printf("5. IZLAZ\n");
+        printf("Unesi svoj izbor: ");
+        do {
+            scanf("%d", &izbor);
+            //printf("Izabrao si: %c", getchar());
+            printf("%d\n", izbor);
+            if (izbor < 1 || izbor >  5) {
+                printf("Neispravan unos. Ponovno unesite izbor: ");
+                isprazniOdabir();
+            }
+        } while (izbor < 1 || izbor > 5);
 
-	switch (choice)
-	{
-	case Igraj:
-		printf("Igram");
-	case DodajIgraca:
-		printf("Dodajem");
-	case IzbrisiIgraca:
-		printf("Brisem");
-	case Izlaz:
-		printf("Izlazim");
-	default:
-		printf("Nista od navedenog");
-		break;
-	}
+        switch (izbor) {
+        case Igraj:
+            printf("Unesi ime igraca: ");
+            scanf("%s", imeIgraca);
+            if (!pronadiIgraca(imeIgraca, &rezultat)) {
+                printf("Igrac ne postoji. Zelis li ga kreirati? (da/ne): ");
+                char response[10];
+                scanf("%9s", response);
+                if (strcmp(response, "da") == 0) {
+                    dodajIgraca(imeIgraca);
+                }
+                else {
+                    break;
+                }
+            }
+            pokreniIgru(&rezultat);
+            azurirajRezultat(imeIgraca, rezultat);
+            break;
+        case DodajIgraca:
+            printf("Unesi ime igraca: ");
+            scanf("%s", imeIgraca);
+            dodajIgraca(imeIgraca);
+            break;
+        case IzbrisiIgraca:
+            printf("Unesi ime igraca za obrisati: ");
+            scanf("%s", imeIgraca);
+            izbrisiIgraca(imeIgraca);
+            break;
+        case IspisIgraca:
+            citajIgrace();
+            sortirajIgrace();
+            ispisSortiranihIgraca();
+            break;;
+        case Izlaz:
+            printf("Izlazenje iz programa.\n");
+            izlazIzPrograma();
+        default:
+            printf("Neispravan unos. Pokusaj ponovno.\n");
+        }
+        isprazniOdabir();
+    }
 
-
-
-
-	return 0;
-	
+    return 0;
 }
